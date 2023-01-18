858135548d6a8babb46c75843cfcc6023f531e97
Dylan Schlichting <dylan.schlichting@tamu.edu>
2023-01-18 14:06:59 -0600

    Delete histograms_whole.py

--- budget_scripts/histograms_whole.py
+++
@@ -1,7 +0,0 @@
-'''
-Computes histograms of vertical relatie vorticity, divergence,
-and strain normalized by the Coriolis parameter for the entire
-water column. Salinity gradient magnitude is calculated as well.
-All quantities are interpolated to psi points horizontally and
-rho points vertically. 
-'''
